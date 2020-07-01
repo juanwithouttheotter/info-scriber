ca294e2cb3a6494bb0c190cefa5bda31d9719d04
Juan Osorio <juan.michael.osorio@gmail.com>
2020-07-01 15:55:07 -0500

    functional api calls

---
+++ routes/apiRoutes.js
@@ -0,0 +1,56 @@
+const noteData = './db/db.json';
+
+exports.route = (app, fs) => {
+
+    app.get('/status', (req, res) => {
+        const status = {
+            status: 'ok info-scriber'
+        }
+        res.json(status);
+    });
+    app.get('/api/notes',  (req, res) => {
+         fs.readFile(noteData,  (err, data) => {
+            if (err) {
+                throw err;
+            }
+             res.send(JSON.parse(data));
+        });
+    });
+
+    app.post('/api/notes', (req, res) => {
+        fs.readFile(noteData, (err, data) => {
+            if (err) {
+                throw err;
+            };
+            const notes = JSON.parse(data);
+            const newNote = req.body;
+            newNote["id"] = notes.length + 1;
+            notes.push(newNote);
+            fs.writeFile(noteData, JSON.stringify(notes,null,4), (err) => {
+                if (err) {
+                    throw err;
+                };
+                console.log('new notes added');
+                res.json({created: true, ...notes});
+            });
+        });
+    });
+    app.delete('/api/notes/:id', (req,res) => {
+        fs.readFile(noteData, (err,data) => {
+            if (err) {
+                throw err;
+            };
+            const notes = JSON.parse(data);
+            const rmId = req.params.id - 1
+            delete notes[rmId];
+            fs.writeFile(noteData, JSON.stringify(notes,null,4), (err) => {
+                if (err) {
+                    throw err;
+                };
+                console.log('note deleted');
+                res.json({deleted: true, ...notes});
+            });
+        });
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = 9000;
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+apiRoutes.route(app,fs);
+htmlRoutes(app);
+
+app.listen(PORT, () => console.log (`API is listening to port ${PORT}`));
